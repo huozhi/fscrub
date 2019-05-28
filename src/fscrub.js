96407f9d619c2d5c429e55f033f4d17d58a7f6e7
huozhi <gilesliu12@gmail.com>
2019-05-28 14:17:34 +0800

    feat: support mouse hover

--- src/fscrub.js
+++ src/fscrub.js
@@ -1,72 +1,91 @@
 function noop() {}
 
+const isPointerSupported = Boolean(window.PointerEvent)
+
+const isMouseTypePointerEvent = e => e.pointerType === 'mouse'
+const isTouchTypePointerEvent = e => e.pointerType === 'touch'
+const isTouchEvent = e => Boolean(window.TouchEvent && e instanceof window.TouchEvent)
+const isMouseEvent = e => Boolean(window.MouseEvent && e instanceof window.MouseEvent)
+
+const pointerScrubEvents = ['pointerdown', 'pointermove', 'pointerup']
+const touchScrubEvents = ['touchstart', 'touchmove', 'touchend']
+const pointerOverEvents = ['pointerenter', 'pointermove', 'pointerleave']
+const mouseOverEvents = ['mouseenter', 'mousemove', 'mouseleave']
+const cancelEvent = isPointerSupported ? 'pointercancel' : 'touchcancel'
+
+function getScrubEvents() {
+  return isPointerSupported ? pointerScrubEvents : touchScrubEvents
+}
+
+function getHoverEvents() {
+  return isPointerSupported ? pointerOverEvents : mouseOverEvents
+}
+
 function fscrub(
   node,
   handlers = {},
   options
 ) {
-  const { onStart = noop, onMove = noop, onEnd = noop } = handlers
-  options = Object.assign({ mouse: false, touch: true }, options)
-  const { mouse, touch } = options
+  const {onStart = noop, onMove = noop, onEnd = noop} = handlers
+  options = Object.assign({hover: false, mouse: false, touch: true}, options)
+
+  const {hover, mouse, touch} = options
+  const isHoverEnabled = Boolean(hover)
   const isMouseEnabled = Boolean(mouse)
   const isTouchEnabled = Boolean(touch)
-  const isMouseTypePointerEvent = e => e.pointerType === 'mouse'
-  const isTouchTypePointerEvent = e => e.pointerType === 'touch'
-  const isTouchEvent = e => Boolean(TouchEvent && e instanceof TouchEvent)
 
-  const isPointerSupported = Boolean(window.PointerEvent)
-  const events = isPointerSupported
-    ? ['pointerdown', 'pointermove', 'pointerup']
-    : ['touchstart', 'touchmove', 'touchend']
-
-  const cancelEvent = isPointerSupported ? 'pointercancel' : 'touchcancel'
+  const events = hover ? getHoverEvents() : getScrubEvents()
+  const endElement = hover ? node : document
 
   const [startEvent, moveEvent, endEvent] = events
-  let scrubbing = false
+  let isMoving = false
 
   function handleStart(e) {
-    scrubbing = true
+    isMoving = true
     if (isMouseEnabled && isMouseTypePointerEvent(e)) onStart(e)
     if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onStart(e)
+    if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onStart(e)
   }
 
   function handleMove(e) {
-    if (scrubbing) {
+    if (isMoving) {
       if (isMouseEnabled && isMouseTypePointerEvent(e)) onMove(e)
       if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onMove(e)
+      if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onMove(e)
     }
   }
 
   function handleEnd(e) {
-    scrubbing = false
+    isMoving = false
     if (isMouseEnabled && isMouseTypePointerEvent(e)) onEnd(e)
     if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onEnd(e)
+    if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onEnd(e)
   }
 
   node.addEventListener(startEvent, handleStart)
   node.addEventListener(moveEvent, handleMove)
-  document.addEventListener(endEvent, handleEnd)
+  endElement.addEventListener(endEvent, handleEnd)
 
-  node.addEventListener(cancelEvent, handleEnd)
+  !hover && node.addEventListener(cancelEvent, handleEnd)
 
   if (!isPointerSupported && isMouseEnabled) {
     node.addEventListener('mousedown', handleStart)
     node.addEventListener('mousemove', handleMove)
-    document.addEventListener('mouseup', handleEnd)
+    endElement.addEventListener('mouseup', handleEnd)
   }
 
   return function release() {
     node.removeEventListener(startEvent, handleStart)
     node.removeEventListener(moveEvent, handleStart)
-    document.removeEventListener(endEvent, handleStart)
+    endElement.removeEventListener(endEvent, handleStart)
 
     if (!isPointerSupported && isMouseEnabled) {
       node.removeEventListener('mousedown', handleStart)
       node.removeEventListener('mousemove', handleMove)
-      document.removeEventListener('mouseup', handleEnd)
+      endElement.removeEventListener('mouseup', handleEnd)
     }
 
-    node.removeEventListener(cancelEvent, handleEnd)
+    !hover && node.removeEventListener(cancelEvent, handleEnd)
   }
 }
 
