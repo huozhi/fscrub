68d6601dc51f0c05563318a5b3620917ced766a1
huozhi <gilesliu12@gmail.com>
2019-05-28 15:41:12 +0800

    fix mouse event still firing after touch pointer events

--- src/fscrub.js
+++ src/fscrub.js
@@ -28,15 +28,17 @@ function fscrub(
 ) {
   const {onStart = noop, onMove = noop, onEnd = noop} = handlers
   options = Object.assign({hover: false, mouse: false, touch: true}, options)
-
+  if (options.hover) {
+    options.touch = options.mouse = false;
+  }
   const {hover, mouse, touch} = options
   const isHoverEnabled = Boolean(hover)
   const isMouseEnabled = Boolean(mouse)
   const isTouchEnabled = Boolean(touch)
-
   const events = hover ? getHoverEvents() : getScrubEvents()
   const endElement = hover ? node : document
 
+  console.log('db:fscrub', node.className, options, events)
   const [startEvent, moveEvent, endEvent] = events
   let isMoving = false
 
@@ -44,14 +46,20 @@ function fscrub(
     isMoving = true
     if (isMouseEnabled && isMouseTypePointerEvent(e)) onStart(e)
     if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onStart(e)
-    if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onStart(e)
+    if (isHoverEnabled && (
+      isMouseTypePointerEvent(e) ||
+      (!isPointerSupported && isMouseEvent(e))
+    )) onStart(e)
   }
 
   function handleMove(e) {
     if (isMoving) {
       if (isMouseEnabled && isMouseTypePointerEvent(e)) onMove(e)
       if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onMove(e)
-      if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onMove(e)
+      if (isHoverEnabled && (
+        isMouseTypePointerEvent(e) ||
+        (!isPointerSupported && isMouseEvent(e))
+      )) onMove(e)
     }
   }
 
@@ -59,7 +67,10 @@ function fscrub(
     isMoving = false
     if (isMouseEnabled && isMouseTypePointerEvent(e)) onEnd(e)
     if (isTouchEnabled && (isTouchTypePointerEvent(e) || isTouchEvent(e))) onEnd(e)
-    if (isHoverEnabled && (isMouseTypePointerEvent(e) || isMouseEvent(e))) onEnd(e)
+    if (isHoverEnabled && (
+      isMouseTypePointerEvent(e) ||
+      (!isPointerSupported && isMouseEvent(e))
+    )) onEnd(e)
   }
 
   node.addEventListener(startEvent, handleStart)
